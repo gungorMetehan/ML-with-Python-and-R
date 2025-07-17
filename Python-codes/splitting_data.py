24ba8d7941d060d1ae8ac3ddf05ddd79030e9f23
Metehan GÜNGÖR <102655648+gungorMetehan@users.noreply.github.com>
2025-07-17 16:44:15 +0300

    Update splitting_data.py

--- Python-codes/splitting_data.py
+++ Python-codes/splitting_data.py
@@ -1,22 +1,12 @@
+from sklearn.datasets import load_iris # data set
+from sklearn.model_selection import train_test_split # data set splitting
 
-from sklearn import datasets
-import pandas as pd
+# data set
+iris = load_iris()
 
-# loading iris data set
-iris = datasets.load_iris()
+# data manipulation
+X = iris.data
+y = iris. target
 
-# converting bunch to data frame
-iris_df = pd.DataFrame(iris.data)
-iris_df['Species'] = iris.target
-
-# missing values?
-iris_df.isnull().sum()
-
-# importing train_test_split() function
-from sklearn.model_selection import train_test_split
-
-# using 80% of dataset as training set and 20% as test set
-trainset, testset = train_test_split(iris_df, test_size = 0.2, random_state = 12)
-
-trainset
-testset
+# data set splitting
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 12)
